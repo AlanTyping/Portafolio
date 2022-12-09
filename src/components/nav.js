02bdcca81ff8cca4b6cf30d5bb2e87179605e88f
unknown <alan.a.n.r.ar@gmail.com>
2022-12-08 21:33:01 -0300

    nuevos cambios

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,7 +7,7 @@ export default function Nav() {
     <>
     <BrowserRouter>
       <nav id='nav' 
-      className='h-[45px] bg-[#00a2ff] text-white fixed bottom-[20px] w-[95%] max-w-[400px] min-w-[300px] flex justify-center
+      className='h-[45px] bg-[#00a2ff] text-white fixed bottom-[20px] w-[80%] max-w-[400px] min-w-[280px] flex justify-center
       justify-around items-center rounded-[20px] z-10 border-[2px]'>
         <div className='h-full'><Link to='#inicio' className='h-full' smooth><h4 className='cursor-pointer h-full flex rounded-[20px] hover:text-[1.1rem] items-center  font-monospace duration-[50ms]'>Inicio</h4></Link></div>
         <div className='h-full'><Link to='#proyectos' className='h-full' smooth><h4 className='cursor-pointer h-full flex rounded-[20px] hover:text-[1.1rem] items-center  font-monospace duration-[50ms]'>Proyectos</h4></Link></div>
