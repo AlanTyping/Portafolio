f77b7f9bf3e4eaf306b1f5c49f3a4a6c147bdcaf
unknown <alan.a.n.r.ar@gmail.com>
2023-02-11 13:52:20 -0300

    changes for responsive

--- src/components/proyects/cards.js
+++ src/components/proyects/cards.js
@@ -7,10 +7,10 @@ export default function Cards({img, name, description, link, opacity}) {
   return (
     <>
     <div className={`h-full w-full flex justify-center items-center flex-col m-1`}>
-        <div className='w-[80%] z-10 min-w-[200px] h-[190px] bg-center bg-contain bg-no-repeat flex justify-center fixed top-0 m-[20px]'>
+        <div className='w-[80%] img z-10 min-w-[200px] h-[190px] bg-center bg-contain bg-no-repeat flex justify-center fixed top-0 m-[20px]'>
           <img src={`${img}`} className='rounded-[15px] border-[2px]'></img>
         </div>
-        <div className={`flex text-start rounded-[10px] proyectos w-full h-[290px] p-[30px] ${opacity} fixed top-[210px] flex-col`}>
+        <div id='card-description' className={`flex text-start rounded-[10px] proyectos w-full h-[290px] p-[30px] ${opacity} fixed top-[210px] flex-col`}>
           <h2 className='text-[2rem] Fjalla-One'>{name}</h2>
           <p className='text-[0.90rem] text-[rgb(234,234,234)]'>{description}</p>
         </div>
