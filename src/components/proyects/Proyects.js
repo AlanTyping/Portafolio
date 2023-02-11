069c08c17f58bc49146b48742e77922599a76638
unknown <alan.a.n.r.ar@gmail.com>
2023-02-11 13:58:49 -0300

    new changes

--- src/components/proyects/Proyects.js
+++ src/components/proyects/Proyects.js
@@ -52,7 +52,7 @@ export default function Proyects() {
     <>
       <section id='proyectos' className='h-screen w-[100%] flex relative flex-col justify-center items-center bg-[#061f2e] text-white'>
         <div className='w-full project-cards absolute top-0 h-[60px] flex items-center justify-center m-3'>
-          <h2 className='text-[2rem] title-query Fjalla-One'>Projects cards</h2>
+          <h2 className='text-[2rem] title-query Fjalla-One'>Projects 🃏</h2>
         </div>
         <Swiper
           effect={"cards"}
