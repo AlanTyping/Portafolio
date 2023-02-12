4ee189648c5acf93555ab0f82bdec71a7a0fc097
unknown <alan.a.n.r.ar@gmail.com>
2023-02-12 14:29:13 -0300

    change for responsive

--- src/components/inicio/Inicio.js
+++ src/components/inicio/Inicio.js
@@ -6,7 +6,7 @@ import alan from './images/alan.jpg';
 export default function Inicio() {
   return (
     <>
-      <section id='home' className='h-[390px] w-[100%] border-b-[10px] border-[var(--orange)] flex justify-center items-center bg-center bg-cover bg-no-repeat'>
+      <section id='home' className='h-[390px] w-[100%] home border-b-[10px] border-[var(--orange)] flex justify-center items-center bg-center bg-cover bg-no-repeat'>
         <div id='home-container' className='w-full home-container h-[380px] flex justify-center items-center absolute top-0 bg-center bg-cover' 
         style={{"backgroundImage": `url(${nieve})`}}>
           <div className={`w-[100%] h-[100%] justify-evenly p-[20px] flex justify-center items-center bg-[rgba(0,0,0,0.60)] text-white flex-col`}>
@@ -15,7 +15,8 @@ export default function Inicio() {
               <h2 className='inicio-h2 text-[2rem] text-[#82c9f1]'>FRONT-END DEVELOPER</h2>
             </div>
             <div 
-              className='h-[180px] img w-[180px] rounded-[10%] border-[4px] bg-center bg-cover bg-no-repeat' 
+              className='h-[180px] w-[180px] rounded-[10%] border-[4px] bg-center bg-cover bg-no-repeat' 
+              id='contact-img'
               style={{"backgroundImage": `url(${alan})`}}>
             </div>
           </div>
--- src/components/nav.js
+++ src/components/nav.js
@@ -9,10 +9,10 @@ export default function Nav() {
       <nav id='nav' 
       className='h-[45px] bg-[#151d28] text-white fixed bottom-[20px] w-[80%] max-w-[400px] min-w-[280px] flex justify-center
       justify-around items-center rounded-[20px] z-10 border-[2px]'>
-        <div className='h-full'><Link to='#home' className='h-full' smooth><h4 className='cursor-pointer h-full flex rounded-[20px] hover:text-[1.1rem] items-center  font-monospace duration-[50ms]'>Home</h4></Link></div>
-        <div className='h-full'><Link to='#sobre-mí' className='h-full' smooth><h4 className='cursor-pointer h-full flex rounded-[20px] hover:text-[1.1rem] items-center  font-monospace duration-[50ms]'>info</h4></Link></div>
-        <div className='h-full'><Link to='#proyectos' className='h-full' smooth><h4 className='cursor-pointer h-full flex rounded-[20px] hover:text-[1.1rem] items-center  font-monospace duration-[50ms]'>Projects</h4></Link></div>
-        <div className='h-full'><Link to='#contacto' className='h-full' smooth><h4 className='cursor-pointer h-full flex rounded-[20px] hover:text-[1.1rem] items-center  font-monospace duration-[50ms]'>Contact</h4></Link></div>
+        <div className='h-full'><Link to='#home' className='h-full' smooth><h4 className='nav-section cursor-pointer h-full flex rounded-[20px] items-center  font-monospace duration-[50ms]'>Home</h4></Link></div>
+        <div className='h-full'><Link to='#sobre-mí' className='h-full' smooth><h4 className='nav-section cursor-pointer h-full flex rounded-[20px] items-center  font-monospace duration-[50ms]'>info</h4></Link></div>
+        <div className='h-full'><Link to='#proyectos' className='h-full' smooth><h4 className='nav-section cursor-pointer h-full flex rounded-[20px] items-center  font-monospace duration-[50ms]'>Projects</h4></Link></div>
+        <div className='h-full'><Link to='#contacto' className='h-full' smooth><h4 className='nav-section cursor-pointer h-full flex rounded-[20px] items-center  font-monospace duration-[50ms]'>Contact</h4></Link></div>
       </nav>
     </BrowserRouter>
     </>
