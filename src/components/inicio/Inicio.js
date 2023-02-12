dfb3be5b3ac93420c92033e941780ae298ffd7eb
unknown <alan.a.n.r.ar@gmail.com>
2023-02-12 18:14:47 -0300

    new changes

--- src/components/inicio/Inicio.js
+++ src/components/inicio/Inicio.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import nieve from './images/nieve.jpg';
-import ojo from './images/ojo.jpg';
-import ojo2 from './images/ojo2.jpg';
 import alan from './images/alan.jpg';
 export default function Inicio() {
   return (
@@ -12,10 +10,10 @@ export default function Inicio() {
           <div className={`w-[100%] h-[100%] justify-evenly p-[20px] flex justify-center items-center bg-[rgba(0,0,0,0.60)] text-white flex-col`}>
             <div className='w-full h-[200px] flex justify-center text-center flex-col'>
               <h1 className='inicio-h1 text-[3.5rem] Fjalla-One'>ALAN ROSALES</h1>
-              <h2 className='inicio-h2 text-[2rem] text-[#82c9f1]'>FRONT-END DEVELOPER</h2>
+              <h2 className='inicio-h2 text-[2rem] text-[#82c9f1]'>FRONTEND DEVELOPER</h2>
             </div>
             <div 
-              className='h-[180px] w-[180px] rounded-[10%] border-[4px] bg-center bg-cover bg-no-repeat' 
+              className='h-[180px] w-[180px] rounded-[10%] border-[1px] bg-center bg-cover bg-no-repeat' 
               id='contact-img'
               style={{"backgroundImage": `url(${alan})`}}>
             </div>
