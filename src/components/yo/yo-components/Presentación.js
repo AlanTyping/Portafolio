42ca6bfb0963d617443b7e0f4a1b0827a977f0da
unknown <alan.a.n.r.ar@gmail.com>
2023-02-12 18:39:56 -0300

    fixing grammatical errors

--- src/components/yo/yo-components/Presentación.js
+++ src/components/yo/yo-components/Presentación.js
@@ -14,13 +14,13 @@ export default function Presentación() {
              text-white flex items-center flex-col text-[1.5rem] justify-start justify-evenly '>
                 <h1 className='Fjalla-One text-[2rem] title-query'>About me 🌘</h1>
                 <p className='Nunito-Sans query'>
-                    I'm an 18 years old guy. Constantly setting different goals
+                    I'm an 18-year-old guy. Constantly setting different goals
                     just by thinking into projects that i consider to be
                     interesting. So, i can optimize my learning process just by actually learning
                     the stuff that i need in order to pursue my goals.
                 </p>
                 <p className='Nunito-Sans query'>
-                    Speaking a bit about me, occasionally, i go out for a run.
+                    Speaking a bit about myself, occasionally, i go out for a run.
                     Often, i interact with people through different communities and that helps me 
                     to be more socially active and learn new things while providing some value. I also like playing piano
                     and solving the rubik's cube, i think that those and also programming, are my main hobbies. 
