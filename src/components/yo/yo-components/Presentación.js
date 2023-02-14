6b2ac4486315c4214ae1df6efd87f374a331aa56
unknown <alan.a.n.r.ar@gmail.com>
2023-02-14 18:31:14 -0300

    new changes

--- src/components/yo/yo-components/Presentación.js
+++ src/components/yo/yo-components/Presentación.js
@@ -6,7 +6,7 @@ export default function Presentación() {
         <section id='sobre-mí' className='h-screen w-[100%] bg-[rgba(0,0,0,0.35)] flex justify-center items-center bg-cover bg-center'>
             <div className='w-[80%] presentacion h-[80%] rounded-[30px] p-[30px] bg-[rgba(0,0,0,0.6)]
              text-white flex items-center flex-col text-[1.5rem] justify-start justify-evenly '>
-                <h1 className='Fjalla-One text-[2rem] title-query'>About me 🌘</h1>
+                <h1 className='Fjalla-One text-[2.4rem] title-query'>About me 🌘</h1>
                 <p className='Nunito-Sans query'>
                     I'm an 18-year-old guy. Constantly setting different goals
                     just by thinking into projects that i consider to be
